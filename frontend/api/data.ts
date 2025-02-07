93d0881092080be4bd871a4cb0ce036063894222
Krishna Kumar <krishnakmar.005work@gmail.com>
2025-02-08 03:33:22 +0530

    third push

---
+++ frontend/api/data.ts
@@ -0,0 +1,49 @@
+export const response = {
+    "data": {
+        "minesNext": {
+            "id": "a8d7fa4d-9b43-4d6b-9b53-00f13ef2d62a",
+            "active": false,
+            "payoutMultiplier": 0,
+            "amountMultiplier": 1,
+            "amount": 0,
+            "payout": 0,
+            "updatedAt": "Wed, 05 Feb 2025 23:05:46 GMT",
+            "currency": "doge",
+            "game": "mines",
+            "user": {
+                "id": "e16a9a64-0fa6-4321-b210-f4d7404a7d27",
+                "name": "Deadpool1555"
+            },
+            "state": {
+                "mines": [
+                    13,
+                    20,
+                    10
+                ],
+                "minesCount": 3,
+                "rounds": [
+                    {
+                        "field": 11,
+                        "payoutMultiplier": 1.125
+                    },
+                    {
+                        "field": 3,
+                        "payoutMultiplier": 1.2857142857142856
+                    },
+                    {
+                        "field": 9,
+                        "payoutMultiplier": 1.4785714285714282
+                    },
+                    {
+                        "field": 5,
+                        "payoutMultiplier": 1.7120300751879696
+                    },
+                    {
+                        "field": 13,
+                        "payoutMultiplier": 0
+                    }
+                ]
+            }
+        }
+    }
+}
--- frontend/components/GameArea/GameArea.tsx
+++ frontend/components/GameArea/GameArea.tsx
@@ -1,6 +1,11 @@
 import GameCard from "./Component/GameCard";
 
 export default function GameArea() {
+  const handleOnclick = (index:number)=>{
+    const loading = true
+    
+  };
+
   return (
     <div className="my-3 p-5 grid grid-cols-5 gap-x-3 gap-y-2 rounded-md bg-C1-dark">
       {Array.from({ length: 25 }).map((_, index) => (
